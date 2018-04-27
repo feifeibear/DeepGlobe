c08ede7a7272afc765af8777049c327f8475c315
jiarui fang <fangjiarui123@gmail.com>
2018-04-27 15:11:24 -0700

    add FCN13

---
+++ models/linknet.py
@@ -0,0 +1,174 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import numpy as np
+
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Activation, UpSampling2D, BatchNormalization, add
+from keras.layers.core import Flatten, Reshape
+from keras.models import Model
+from keras.regularizers import l2
+import keras.backend as K
+from keras.optimizers import Adam
+
+def jaccard_coef(y_true, y_pred):
+    smooth = 1e-12
+    # intersection = K.sum(y_true * y_pred, axis=[0, -1, -2])
+    # sum_ = K.sum(y_true + y_pred, axis=[0, -1, -2])
+
+    intersection = K.sum(y_true * y_pred, axis=[0, -2, -3])
+    sum_ = K.sum(y_true + y_pred, axis=[0, -2, -3])
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return K.mean(jac)
+
+
+def jaccard_coef_int(y_true, y_pred):
+    smooth = 1e-12
+    y_pred_pos = K.round(K.clip(y_pred, 0, 1))
+    #intersection = K.sum(y_true * y_pred_pos, axis=[0, -1, -2])
+    #sum_ = K.sum(y_true + y_pred_pos, axis=[0, -1, -2])
+    intersection = K.sum(y_true * y_pred_pos, axis=[0, -2, -3])
+    sum_ = K.sum(y_true + y_pred_pos, axis=[0, -2, -3])
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return K.mean(jac)
+
+
+
+def _shortcut(input, residual):
+    """Adds a shortcut between input and residual block and merges them with "sum"
+    """
+    # Expand channels of shortcut to match residual.
+    # Stride appropriately to match residual (width, height)
+    # Should be int if network architecture is correctly configured.
+    input_shape = K.int_shape(input)
+    residual_shape = K.int_shape(residual)
+    stride_width = int(round(input_shape[1] / residual_shape[1]))
+    stride_height = int(round(input_shape[2] / residual_shape[2]))
+    equal_channels = input_shape[3] == residual_shape[3]
+
+    shortcut = input
+    # 1 X 1 conv if shape is different. Else identity.
+    if stride_width > 1 or stride_height > 1 or not equal_channels:
+        shortcut = Conv2D(filters=residual_shape[3],
+                          kernel_size=(1, 1),
+                          strides=(stride_width, stride_height),
+                          padding="valid",
+                          kernel_initializer="he_normal",
+                          kernel_regularizer=l2(0.0001))(input)
+
+    return add([shortcut, residual])
+
+def encoder_block(input_tensor, m, n):
+    x = BatchNormalization()(input_tensor)
+    x = Activation('relu')(x)
+    x = Conv2D(filters=n, kernel_size=(3, 3), strides=(2, 2), padding="same")(x)
+
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters=n, kernel_size=(3, 3), padding="same")(x)
+
+    added_1 = _shortcut(input_tensor, x)
+
+    x = BatchNormalization()(added_1)
+    x = Activation('relu')(x)
+    x = Conv2D(filters=n, kernel_size=(3, 3), padding="same")(x)
+
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters=n, kernel_size=(3, 3), padding="same")(x)
+
+    added_2 = _shortcut(added_1, x)
+
+    return added_2
+
+def decoder_block(input_tensor, m, n):
+    x = BatchNormalization()(input_tensor)
+    x = Activation('relu')(x)
+    x = Conv2D(filters=int(m/4), kernel_size=(1, 1))(x)
+
+    x = UpSampling2D((2, 2))(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters=int(m/4), kernel_size=(3, 3), padding='same')(x)
+
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters=n, kernel_size=(1, 1))(x)
+
+    return x
+
+def LinkNet(input_shape=(256, 256, 8), classes=1):
+    inputs = Input(shape=input_shape)
+
+    x = BatchNormalization()(inputs)
+    x = Activation('relu')(x)
+    x = Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2))(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding="same")(x)
+
+    encoder_1 = encoder_block(input_tensor=x, m=64, n=64)
+
+    encoder_2 = encoder_block(input_tensor=encoder_1, m=64, n=128)
+
+    encoder_3 = encoder_block(input_tensor=encoder_2, m=128, n=256)
+
+    encoder_4 = encoder_block(input_tensor=encoder_3, m=256, n=512)
+
+    decoder_4 = decoder_block(input_tensor=encoder_4, m=512, n=256)
+
+    decoder_3_in = add([decoder_4, encoder_3])
+    decoder_3_in = Activation('relu')(decoder_3_in)
+
+    decoder_3 = decoder_block(input_tensor=decoder_3_in, m=256, n=128)
+
+    decoder_2_in = add([decoder_3, encoder_2])
+    decoder_2_in = Activation('relu')(decoder_2_in)
+
+    decoder_2 = decoder_block(input_tensor=decoder_2_in, m=128, n=64)
+
+    decoder_1_in = add([decoder_2, encoder_1])
+    decoder_1_in = Activation('relu')(decoder_1_in)
+
+    decoder_1 = decoder_block(input_tensor=decoder_1_in, m=64, n=64)
+
+    x = UpSampling2D((2, 2))(decoder_1)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters=32, kernel_size=(3, 3), padding="same")(x)
+
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters=32, kernel_size=(3, 3), padding="same")(x)
+
+    x = UpSampling2D((2, 2))(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters=classes, kernel_size=(2, 2), padding="same")(x)
+
+    model = Model(inputs=inputs, outputs=x)
+
+    model.compile(optimizer=Adam(),
+             loss='binary_crossentropy',
+             metrics=['accuracy', jaccard_coef, jaccard_coef_int])
+    return model
+
+def get_linknet():
+    return LinkNet(classes=1)
+
+if __name__ == '__main__':
+    X_trn = np.random.randn(803, 256, 256, 8)
+    y_trn = np.random.randn(803, 256, 256, 1)
+    X_val = np.random.randn(345, 256, 256, 8)
+    y_val = np.random.randn(345, 256, 256, 1)
+
+    model = get_linknet()
+    model.fit(
+        X_trn, y_trn,
+        batch_size=8,
+        nb_epoch=1, # 200 originally
+        shuffle=True,
+        verbose=1)
+#        validation_data=(X_val, y_val))
+#        callbacks=[model_checkpoint, model_earlystop, model_history])
+
