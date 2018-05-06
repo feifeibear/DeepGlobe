a6920a773aa42e934cecffe13b90d15b4cfef091
jiarui fang <fangjiarui123@gmail.com>
2018-05-06 09:38:33 -0700

    batch norm

--- v9s.py
+++ v9s.py
@@ -5,6 +5,7 @@ v9s model
 * Input: v5_im
 
 Author: Kohei <i@ho.lc>
+feifeibear <>
 """
 from logging import getLogger, Formatter, StreamHandler, INFO, FileHandler
 from pathlib import Path
@@ -25,7 +26,7 @@ import tables as tb
 import pandas as pd
 import numpy as np
 from keras.models import Model
-from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose, BatchNormalization, Activation
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 from keras.callbacks import ModelCheckpoint, EarlyStopping, History
@@ -741,9 +742,103 @@ def get_linknet():
 def get_fcn32():
     return FCN32(n_classes=1)
 
+def get_unet_bn():
+#    inputs = Input((8, img_rows, img_cols))
+    # inputs = Input((8, 256, 256))
+    myaxis = 3
+    inputs = Input((256, 256, 8))
+    conv1 = Conv2D(32, (3, 3), padding='same', use_bias=False)(inputs)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Activation("relu")(conv1)
+
+    conv1 = Conv2D(32, (3, 3), use_bias=False, padding='same')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Activation("relu")(conv1)
+
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), use_bias=False, padding='same')(pool1)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Activation("relu")(conv2)
+    conv2 = Conv2D(64, (3, 3), use_bias=False, padding='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Activation("relu")(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), use_bias=False, padding='same')(pool2)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Activation("relu")(conv3)
+    conv3 = Conv2D(128, (3, 3), use_bias=False, padding='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Activation("relu")(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), use_bias=False, padding='same')(pool3)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = Activation("relu")(conv4)
+    conv4 = Conv2D(256, (3, 3), use_bias=False, padding='same')(conv4)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = Activation("relu")(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(512, (3, 3), use_bias=False, padding='same')(pool4)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = Activation("relu")(conv5)
+    conv5 = Conv2D(512, (3, 3), use_bias=False, padding='same')(conv5)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = Activation("relu")(conv5)
+
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=myaxis)
+    conv6 = Conv2D(256, (3, 3), use_bias=False, padding='same')(up6)
+    conv6 = BatchNormalization()(conv6)
+    conv6 = Activation("relu")(conv6)
+    conv6 = Conv2D(256, (3, 3), use_bias=False, padding='same')(conv6)
+    conv6 = BatchNormalization()(conv6)
+    conv6 = Activation("relu")(conv6)
+
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=myaxis)
+    conv7 = Conv2D(128, (3, 3), use_bias=False, padding='same')(up7)
+    conv7 = BatchNormalization()(conv7)
+    conv7 = Activation("relu")(conv7)
+    conv7 = Conv2D(128, (3, 3), use_bias=False, padding='same')(conv7)
+    conv7 = BatchNormalization()(conv7)
+    conv7 = Activation("relu")(conv7)
+
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=myaxis)
+    conv8 = Conv2D(64, (3, 3), use_bias=False, padding='same')(up8)
+    conv8 = BatchNormalization()(conv8)
+    conv8 = Activation("relu")(conv8)
+    conv8 = Conv2D(64, (3, 3), use_bias=False, padding='same')(conv8)
+    conv8 = BatchNormalization()(conv8)
+    conv8 = Activation("relu")(conv8)
+
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=myaxis)
+    conv9 = Conv2D(32, (3, 3), use_bias=False, padding='same')(up9)
+    conv9 = BatchNormalization()(conv9)
+    conv9 = Activation("relu")(conv9)
+    conv9 = Conv2D(32, (3, 3), use_bias=False, padding='same')(conv9)
+    conv9 = BatchNormalization()(conv9)
+    conv9 = Activation("relu")(conv9)
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+
+    model.compile(optimizer=Adam(),
+                   loss='binary_crossentropy',
+                   metrics=['accuracy', jaccard_coef, jaccard_coef_int])
+
+#   model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
+#                   metrics=['accuracy', jaccard_coef, jaccard_coef_int])
+
+    return model
+
+
+
 def get_unet():
 #    inputs = Input((8, img_rows, img_cols))
     # inputs = Input((8, 256, 256))
+
     myaxis = 3
     inputs = Input((256, 256, 8))
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
@@ -1148,7 +1243,7 @@ def _internal_test_predict_best_param(area_id,
     fn = FMT_TESTPRED_PATH.format(prefix)
     fn_model = FMT_VALMODEL_PATH.format(prefix + '_{epoch:02d}')
     fn_model = fn_model.format(epoch=epoch)
-    model = get_unet()
+    model = get_unet_bn()
     model.load_weights(fn_model)
 
     fn_test = FMT_TEST_IMAGELIST_PATH.format(prefix=prefix)
@@ -1233,7 +1328,7 @@ def validate_score(area_id):
 
         # Load model weights
         # Predict and Save prediction result
-        model = get_unet()
+        model = get_unet_bn()
         model.load_weights(FMT_VALMODEL_PATH.format(prefix))
         y_pred = model.predict(X_val - X_mean, batch_size=8, verbose=1)
         del model
@@ -1512,7 +1607,7 @@ def predict(area_id):
 
     # Load model weights
     # Predict and Save prediction result
-    model = get_unet()
+    model = get_unet_bn()
     model.load_weights(FMT_VALMODEL_PATH.format(prefix))
     y_pred = model.predict(X_test - X_mean, batch_size=8, verbose=1)
     del model
@@ -1551,7 +1646,7 @@ def _internal_validate_predict(area_id,
     # Predict and Save prediction result
     fn_model = FMT_VALMODEL_PATH.format(prefix + '_{epoch:02d}')
     fn_model = fn_model.format(epoch=epoch)
-    model = get_unet()
+    model = get_unet_bn()
     model.load_weights(fn_model)
 
     fn_test = FMT_VALTEST_IMAGELIST_PATH.format(prefix=prefix)
@@ -1753,9 +1848,9 @@ def validate(datapath):
     X_trn = X_trn - X_mean
 
     use_channel_last = True
-    model_name = "deeplab"
+    model_name = "unet"
     if model_name == "unet":
-        model = get_unet()
+        model = get_unet_bn()
     elif model_name == "linknet":
         model = get_linknet()
     elif model_name == "deeplab":
